aa587388a1cb8b36d1590ab63a8a6bd819f8ea3d
tonnisje <tonnisje@mail.uc.edu>
2026-06-11 19:23:32 -0400

    Added JS func and Cookie update

--- js/cookie.js
+++ js/cookie.js
@@ -2,3 +2,5 @@ if (document.cookie.indexOf("username") <0 ){
     alert('Welcome to my homepage for the first time!');
 }else
     alert("Welcome back! Your last visit was " + document.cookie);
+
+document.cookie = "lastVisit=" + new Date().toString() + ";path=/";
